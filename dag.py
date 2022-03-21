05096a15393fd8802e6bae88ffeeddc51c34a980
Aminder-Khungura <khunguraaminder@gmail.com>
2022-03-20 19:23:26 -0600

    Update

--- dag.py
+++ dag.py
@@ -1,24 +1,56 @@
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-from airflow.contrib.operators.mysql_to_gcs import MySqlToGoogleCloudStorageOperator
-from airflow.operators.mysql_operator import mysql_conn_id
+from airflow.operators.python import PythonOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryUpdateTableOperator
 from datetime import datetime
+import mysql.connector
 
 
-connection_id = mysql_conn_id(Host='localhost', Login='root', Password='root', Port=3306)
+def read_data():
+    database = mysql.connector.connect(host='localhost', user='root', password='pass', port='3306', database='sensor_data', auth_plugin='mysql_native_password')
+    cursor = database.cursor()
+    cursor.execute('SELECT * FROM data')
+    data = cursor.fetchall()
+    cursor.close();
+    database.close()
+
+    date = []
+    temperature = []
+    heart_rate = []
+    for i in range(len(data)):
+        date.append(data[i][0])
+        temperature.append(data[i][1])
+        heart_rate.append(data[i][2])
+    return date, temperature, heart_rate
 
 
-def convert_str(x):
-    return float(x)
+def process_data(ti):
+    values = ti.xcom_pull(task_ids='Read Values From MySQL')
+    date = values[0].apply(lambda x: x.strftime("%m/%d/%Y, %H:%M:%S"))
+    temperature = values[1].apply(lambda x: float(x))
+    heart_rate = values[2].apply(lambda x: float(x))
+    return date, temperature, heart_rate
 
 
-with DAG('Sensor_dag', start_date=datetime(2021, 1, 1), schedule_interval='@daily', catchup=False) as dag:
-
-    Task_1 = MySqlOperator(task_id='get_data', sql=r"""SELECT * FROM Sensor_DB;""", dag=dag)
-
-    Task_2 = PythonOperator(
-        task_id='Convert str to float',
-        python_callable=convert_str()
+with DAG(
+    'sensor_dag',
+    start_date=datetime(2021,1,1),
+    schedule_interval='@daily',
+    catchup=False
+) as dag:
+    read_data = PythonOperator(
+        task_id='Read Values From MySQL',
+        python_callable=read_data
     )
 
-    Task_3 = MySqlToGoogleCloudStorageOperator(mysql_conn_id='connection_id', ensure_utc=False, *args, **kwargs)
+    data_conversion = PythonOperator(
+        task_id='Process Data',
+        python_callable=process_data
+    )
+
+    upload_data = BigQueryUpdateTableOperator(
+        task_id='Load Data Into BigQuery',
+        dataset_id=sensor_data,
+        dataset_resource={"description": "Updated dataset"},
+    )
+
+    read_data >> data_conversion >> upload_data
